7a9017130a3f71f54ffd7a3175b0f04387943443
Víctor Andújar <v.andujar@fruntera.com>
2024-05-24 19:02:07 +0200

    Create thunk to fetch async data & add its testing at movies module

--- src/modules/movies/infrastructure/moviesServicesRepo.test.ts
+++ src/modules/movies/infrastructure/moviesServicesRepo.test.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import customFetch from "@/sections/shared/utils/customFetch/customFetch";
-import { getNowPlayingMovies } from "./moviesServicesRepository";
+import { getMovieById, getNowPlayingMovies } from "./moviesServicesRepository";
 import { moviesReducer } from "@/store/features/moviesSlice/moviesSlice";
+import { mockMovie } from "@/mocks/moviesMocks";
 
 jest.mock("../../../sections/shared/utils/customFetch/customFetch");
 
@@ -50,6 +51,40 @@ describe("Given a getNowPlayingMovies function", () => {
 
       expect(response.payload).toEqual({
         error: "Falided to load",
+        success: false,
+      });
+    });
+  });
+});
+
+describe("Given a getMovieById function", () => {
+  describe("When it is called and it is fulfilled", () => {
+    test("should fetch movie by ID successfully", async () => {
+      (customFetch as jest.Mock).mockResolvedValue({
+        success: true,
+        data: mockMovie,
+      });
+
+      const response = await store.dispatch(getMovieById({ id: 786892 }));
+
+      expect(response.payload).toEqual({
+        success: true,
+        data: mockMovie,
+      });
+    });
+  });
+
+  describe("When it is called and the response is rejected", () => {
+    test("should handle fetch error", async () => {
+      (customFetch as jest.Mock).mockResolvedValue({
+        success: false,
+        error: "Failed to load movie",
+      });
+
+      const response = await store.dispatch(getMovieById({ id: 786892 }));
+
+      expect(response.payload).toEqual({
+        error: "Failed to load movie",
         success: false,
       });
     });
--- src/modules/movies/infrastructure/moviesServicesRepository.ts
+++ src/modules/movies/infrastructure/moviesServicesRepository.ts
@@ -1,11 +1,11 @@
 import endpoints from "@/sections/shared/utils/api/endpoints/endpoints";
 import customFetch from "@/sections/shared/utils/customFetch/customFetch";
-import { NowPlayingMoviesApiResponse } from "../domain/Movies";
+import { Movie, NowPlayingMoviesApiResponse } from "../domain/Movies";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { MoviesRepository } from "../domain/MoviesRepository";
 
 export const moviesRepository = (): MoviesRepository => {
-  return { getNowPlayingMovies };
+  return { getNowPlayingMovies, getMovieById };
 };
 
 export const getNowPlayingMovies = createAsyncThunk<
@@ -24,4 +24,22 @@ export const getNowPlayingMovies = createAsyncThunk<
   }
 
   return { data: data as NowPlayingMoviesApiResponse, success };
+});
+
+export const getMovieById = createAsyncThunk<
+  { data?: Movie; success?: boolean; error?: string },
+  { id: number }
+>("movies/getMovieById", async ({ id }) => {
+  const { data, success, error } = await customFetch(
+    "GET",
+    `${endpoints.getMovieById}/${id}`,
+  );
+  if (!success) {
+    return {
+      success,
+      error,
+    };
+  }
+
+  return { data: data as Movie, success };
 });
