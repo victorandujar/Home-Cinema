49ce5938ba5a426c609e8c2e810cd21838efe208
Víctor Andújar <v.andujar@fruntera.com>
2024-05-24 15:53:23 +0200

    Create MovieCardList component, add its styles & testing

---
+++ src/sections/movies/components/MovieCardList/MovieCardList.tsx
@@ -0,0 +1,17 @@
+import { useAppSelector } from "@/store/hooks";
+import React from "react";
+import styles from "./MovieCardList.module.scss";
+import MovieCard from "../MovieCard/MovieCard";
+
+export const MovieCardList = () => {
+  const { movies } = useAppSelector((state) => state.movies);
+  return (
+    <ul className={styles.container}>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </ul>
+  );
+};
