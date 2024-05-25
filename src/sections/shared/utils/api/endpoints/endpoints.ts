f1c4fb7681de4cab4af0d1b2618c4406277e293a
Víctor Andújar <v.andujar@fruntera.com>
2024-05-25 18:28:10 +0200

    Modify renderWithProviders func with StoreProvider

--- src/sections/shared/utils/api/endpoints/endpoints.ts
+++ src/sections/shared/utils/api/endpoints/endpoints.ts
@@ -3,7 +3,9 @@ import environments from "../../environments/environments";
 const endpoints = {
   nowPlayingMovies: `${environments.apiUrl}movie/now_playing`,
   getMovieById: `${environments.apiUrl}movie`,
-  getUserSession: `${environments.apiUrl}authentication/guest_session/new`,
+  getRequestToken: `${environments.apiUrl}authentication/token/new`,
+  createSession: `${environments.apiUrl}authentication/session/new`,
+  validateCredentials: `${environments.apiUrl}authentication/token/validate_with_login`,
 };
 
 export default endpoints;
